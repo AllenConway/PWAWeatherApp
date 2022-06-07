df2e68ca79ac5afd242458a41cc09409bc536426
Allen Conway <allen.conway@softvision.com>
2022-06-07 00:17:11 -0400

    Added Capacitor and Android App, removed legacy packages and added proper linting

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.PWAWeatherApp',
+  appName: 'PWAWeatherApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
