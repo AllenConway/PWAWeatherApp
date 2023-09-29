e40eaefd5c602caf30b1b20578c0fc0cd2065697
Allen Conway <aconway@xpirit.com>
2023-09-29 10:30:31 -0400

    Upgraded to Angular 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,15 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, 
-            HttpClientModule,
-            IonicModule.forRoot(), 
-            AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule,
+        HttpClientModule,
+        IonicModule.forRoot(),
+        AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
